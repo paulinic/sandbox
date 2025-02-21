b12d79fd2f32bfa361b1d0a0677bb499eed48e25
paulinic <nicole.pauli.89@gmail.com>
2025-02-21 13:34:27 +0100

    New code examples and robot.txt file as an example

--- testfile_for_github.R
+++ testfile_for_github.R
@@ -1,2 +1,21 @@
 #new script after failing the old one 
 1+1
+
+library(rvest)
+library(tidyverse)
+library(httr)
+
+#browseURL("https://www.nzz.ch/robots.txt")
+#download.file("https://www.nzz.ch", destfile = "nzz.html")
+
+#url <- "https://www.20min.ch/"
+
+read_html(url) %>% 
+  html_elements(css = ".kilkaya-teaser-title") %>%
+  html_text()
+
+url <- "https://www.unilu.ch/"
+page <- read_html(url)
+
+page_new <- httr::GET(url)
+content(page_new)
